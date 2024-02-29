e8d0ed14e85481648b16b4161889c61d4f7995f6
OleksaSnuff <alex1204park@gmail.com>
2024-02-29 23:25:15 +0200

    12

--- src/js/onformsubmit.js
+++ src/js/onformsubmit.js
@@ -1,14 +1,13 @@
-import { refs } from "./refs";
-import { createNoteMarkup } from "./createNoteMarkup";
+import { refs } from './refs';
+import { createNoteMarkup } from './createNoteMarkup';
 export function onFormSubmit(event) {
-    event.preventDefault();
-    const newNote = {}
-    new FormData(event.currentTarget).forEach((value, key) => {
-        newNote[key] = value;
-        
-    })
-    console.log(newNote)
+  event.preventDefault();
+  const newNote = {};
+  new FormData(event.currentTarget).forEach((value, key) => {
+    newNote[key] = value;
+  });
+  console.log(newNote);
 
-    const markup = createNoteMarkup(newNote);
-    refs.taskList.insertAdjacentHTML('beforeend', markup);
+  const markup = createNoteMarkup(newNote);
+  refs.taskList.insertAdjacentHTML('beforeend', markup);
 }
